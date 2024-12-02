b2340599018d1a669afb5aaa7ee546060fe26bac
Bernhard M. Wiedemann <bwiedemann@suse.de>
2024-12-02 07:28:09 +0100

    Add rbcheck2 wrapper

---
+++ rbcheck2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# find all staging projects and run rbcheck1.sh on each of them
+prjs=$(osc api "/search/project?match=starts_with(@name,'openSUSE:Factory:Staging:') and contains(@name,':adi:')" | perl -ne 'm/^<project name="([^"]+)">/ && print $1,"\n"')
+echo "found $prjs"
+for prj in $prjs ; do
+    echo "checking $prj for reproducible builds"
+    ./rbcheck1.sh "$prj"
+done
