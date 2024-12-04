effcdae9bccc7d0ed0fc06a35ba49ee46dd95fe4
Bernhard M. Wiedemann <bwiedemann@suse.de>
2024-12-04 08:08:04 +0100

    Allow leading space/indent
    sometimes OBS returns it and sometimes it does not.

--- rbcheck2.sh
+++ rbcheck2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # find all staging projects and run rbcheck1.sh on each of them
-prjs=$(osc api "/search/project?match=starts_with(@name,'openSUSE:Factory:Staging:') and contains(@name,':adi:')" | perl -ne 'm/^<project name="([^"]+)">/ && print $1,"\n"')
+prjs=$(osc api "/search/project?match=starts_with(@name,'openSUSE:Factory:Staging:') and contains(@name,':adi:')" | perl -ne 'm/^\s*<project name="([^"]+)">/ && print $1,"\n"')
 echo "found $prjs"
 for prj in $prjs ; do
     echo "checking $prj for reproducible builds"
