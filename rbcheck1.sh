df5d635acf07b77169c854ca4ef3392c01c5ca21
Bernhard M. Wiedemann <bwiedemann@suse.de>
2025-11-12 10:28:54 +0000

    Use rdelete --force
    to reduce the number of API-calls

--- rbcheck1.sh
+++ rbcheck1.sh
@@ -16,7 +16,7 @@ function test_api_exists
 function cleanup
 {
     local prj=$1
-    $dry osc rdelete -m drop -r "$prj" | tee -a .cleanup
+    $dry osc rdelete --force -m drop -r "$prj" | tee -a .cleanup
 }
 
 
